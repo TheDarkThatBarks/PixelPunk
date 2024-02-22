27e23990e4e33de4c7d15fe40764ca2d45a32ae0
TheDarkThatBarks <125921991+TheDarkThatBarks@users.noreply.github.com>
2024-02-22 14:30:57 -0500

    Changed screen rendering

--- main.cpp
+++ main.cpp
@@ -10,6 +10,13 @@
 #include <vector>
 #include <cmath>
 
+#define KB_UP 72
+#define KB_DOWN 80
+#define KB_LEFT 75
+#define KB_RIGHT 77
+#define KB_ESCAPE 27
+#define KB_SPACE 32
+
 HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 HWND console = GetConsoleWindow();
 RECT r;
@@ -30,73 +37,37 @@ void setCursor(short x, short y) {
 
 void setColor(std::string background, std::string text) {
     int code = 0;
-    if (background == "DARK_BLUE") {
-        code += 16*1;
-    } else if (background == "GREEN") {
-        code += 16*2;
-    } else if (background == "LIGHT_BLUE") {
-        code += 16*3;
-    } else if (background == "RED") {
-        code += 16*4;
-    } else if (background == "PURPLE") {
-        code += 16*5;
-    } else if (background == "YELLOW") {
-        code += 16*6;
-    } else if (background == "LIGHT_GRAY") {
-        code += 16*7;
-    } else if (background == "DARK_GRAY") {
-        code += 16*8;
-    } else if (background == "BLUE") {
-        code += 16*9;
-    } else if (background == "LIGHT_GREEN") {
-        code += 16*10;
-    } else if (background == "LIGHTEST_BLUE") {
-        code += 16*11;
-    } else if (background == "LIGHT_RED") {
-        code += 16*12;
-    } else if (background == "LIGHT_PURPLE") {
-        code += 16*13;
-    } else if (background == "LIGHT_YELLOW") {
-        code += 16*14;
-    } else if (background == "WHITE") {
-        code += 16*15;
+    const std::string colors[16] = {"BLACK", "DARK_BLUE", "GREEN", "LIGHT_BLUE", "RED", "PURPLE", "YELLOW", "LIGHT_GRAY", "DARK_GRAY",
+                                    "BLUE", "LIGHT_GREEN", "LIGHTEST_BLUE", "LIGHT_RED", "LIGHT_PURPLE", "LIGHT_YELLOW", "WHITE"};
+    int bgCode = 0, tCode = 0;
+    for (int i = 0; i < 16; i++) {
+        if (colors[i] == background)
+            bgCode = i;
+        if (colors[i] == text)
+            tCode = i;
     }
-    if (text == "DARK_BLUE") {
-        code += 1;
-    } else if (text == "GREEN") {
-        code += 2;
-    } else if (text == "LIGHT_BLUE") {
-        code += 3;
-    } else if (text == "RED") {
-        code += 4;
-    } else if (text == "PURPLE") {
-        code += 5;
-    } else if (text == "YELLOW") {
-        code += 6;
-    } else if (text == "LIGHT_GRAY") {
-        code += 7;
-    } else if (text == "DARK_GRAY") {
-        code += 8;
-    } else if (text == "BLUE") {
-        code += 9;
-    } else if (text == "LIGHT_GREEN") {
-        code += 10;
-    } else if (text == "LIGHTEST_BLUE") {
-        code += 11;
-    } else if (text == "LIGHT_RED") {
-        code += 12;
-    } else if (text == "LIGHT_PURPLE") {
-        code += 13;
-    } else if (text == "LIGHT_YELLOW") {
-        code += 14;
-    } else if (text == "WHITE") {
-        code += 15;
-    }
+    code = bgCode * 16 + tCode;
     SetConsoleTextAttribute(hConsole, code);
+}
+
+void setColor(char ch) {
+    switch (ch) {
+        case '+':
+            setColor("PURPLE", "PURPLE");
+            break;
+        default:
+            setColor("BLACK", "LIGHT_GRAY");
+    }
 }
 
 std::vector<std::vector<int>> mapCoord;
 int rows, cols;
+int rOffset = 3;
+int cOffset = 6;
+struct {
+    int r;
+    int c;
+} playerPos;
 
 void initMap(std::string map) {
     rows = 1;
@@ -119,6 +90,8 @@ void initMap(std::string map) {
             switch (ch) {
                 case '+':
                     val = 1;
+                    playerPos.r = r;
+                    playerPos.c = c / 2;
                     break;
                 case '*':
                     val = -1;
@@ -131,56 +104,45 @@ void initMap(std::string map) {
         }
         r++;
     }
+    setCursor(cOffset, rOffset - 1);
+    for (int c = 0; c < cols; c++)
+        std::cout << "__";
+    for (int r = 0; r < rows; r++) {
+        setCursor(cOffset - 1, rOffset + r);
+        std::string str = "|";
+        for (int c = 0; c < cols; c++)
+            str += r == rows - 1 ? "__" : "  ";
+        std::cout << str << "|";
+    }
 }
 
 void printScreen() {
-    int rOffset = 3;
-    int cOffset = 5;
-    for (int r = -1; r < rows; r++) {
+    for (int r = 0; r < rows; r++) {
         setCursor(cOffset, rOffset + r);
         setColor("BLACK", "LIGHT_GRAY");
-        if (r == -1) {
-            std::cout << " ";
-        } else {
-            std::cout << "|";
-        }
         for (int c = 0; c < cols; c++) {
-            if (r == -1) {
-                std::cout << "__";
-            } else {
-                char ch = ' ';
-                switch (mapCoord[r][c]) {
-                    case 1:
-                        ch = '+';
-                        setColor("PURPLE", "PURPLE");
-                        break;
-                    case -1:
-                        ch = '*';
-                        setColor("WHITE", "WHITE");
-                        break;
-                    case -5:
-                        ch = '-';
-                        setColor("LIGHT_RED", "LIGHT_RED");
-                        break;
-                    default:
-                        if (r == rows - 1)
-                            ch = '_';
-                        setColor("BLACK", "LIGHT_GRAY");
-                }
-                std::cout << ch << ch;
+            switch (mapCoord[r][c]) {
+                case 1:
+                    setColor("PURPLE", "PURPLE");
+                    break;
+                case -1:
+                    setColor("WHITE", "WHITE");
+                    break;
+                case -5:
+                    setColor("LIGHT_RED", "LIGHT_RED");
+                    break;
+                default:
+                    setColor("BLACK", "LIGHT_GRAY");
             }
+            std::cout << (r == rows - 1 ? "__" : "  ");
         }
         setColor("BLACK", "LIGHT_GRAY");
-        if (r == -1) {
-            std::cout << " ";
-        } else {
-            std::cout << "|";
-        }
-        //std::cout << std::endl;
     }
-    //setCursor(0, 0);
+    setCursor(0, 0);
     setColor("BLACK", "LIGHT_GRAY");
 }
+
+void keyPress();
 
 int main() {
     GetWindowRect(console, &r);
@@ -190,4 +152,53 @@ int main() {
 	std::string map((std::istreambuf_iterator<char>(m)), std::istreambuf_iterator<char>());
     initMap(map);
     printScreen();
+    keyPress();
+}
+
+void updateDisplay(char ch, int oldR, int oldC, int newR, int newC) {
+    setCursor(cOffset + oldC * 2, rOffset + oldR);
+    setColor(' ');
+    std::cout << (oldR == rows - 1 ? "__" : "  ");
+    setCursor(cOffset + newC * 2, rOffset + newR);
+    setColor(ch);
+    std::cout << ch << ch;
+    setCursor(0, 0);
+    setColor(' ');
+}
+
+void changePos(int rChange, int cChange) {
+    int newR = playerPos.r + rChange;
+    int newC = playerPos.c + cChange;
+    if (newR >= 0 && newR < rows && newC >= 0 && newC < cols && mapCoord[newR][newC] >= 0) {
+        mapCoord[playerPos.r][playerPos.c] = 0;
+        mapCoord[newR][newC] = 1;
+        updateDisplay('+', playerPos.r, playerPos.c, newR, newC);
+        playerPos.r = newR;
+        playerPos.c = newC;
+    }
+}
+
+void keyPress() {
+    int kbCode;
+    while (kbCode != KB_ESCAPE) {
+        if (kbhit()) {
+            kbCode = getch();
+            int rChange = 0, cChange = 0;
+            switch (kbCode) {
+                case KB_UP:
+                    rChange--;
+                    break;
+                case KB_DOWN:
+                    rChange++;
+                    break;
+                case KB_LEFT:
+                    cChange--;
+                    break;
+                case KB_RIGHT:
+                    cChange++;
+                    break;
+            }
+            changePos(rChange, cChange);
+        }
+    }
 }
